4681ffd937d95c200a4cef66cda67a13101a1ee6
ahmedccript <murtazakhan2595@gmail.com>
2024-04-16 23:32:20 +0500

    Installed chadcn UI library and set up the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Welcome to pizza shop</h1>
+      npx shadcn-ui@latest init
+      <Button variant="outline">Button</Button>
+    </main>
+  );
+}
